3ff508b461c5230a9386b7d6c9e9ec454c1180ad
Thandriss <mahonko_yana@mail.ru>
2023-11-08 16:07:21 +0200

    new

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ let msg = {
 
 let list = [{id: "dog"}, {id: "cat"}]
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser')
 
 app.get("/hello", (req, res) => {
     res.json(msg);
@@ -17,10 +19,24 @@ app.get("/echo/:id", (req, res) => {
     res.json(el);
 })
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.json()) ;
+app.use(bodyParser.urlencoded({ extended: true })) ;
+
 app.post("/sum", function(req, res) {
     console.log('receiving data ...');
-    console.log('body is ',req.body);
-    res.send(req.body);
+    const { numbers } = req.body  
+    console.log(numbers[2])
+    let MySum = 0;
+    for(el in numbers){
+        MySum += numbers[el];
+        console.log(numbers[el])
+    }
+    let sumRes = {
+        sum: MySum
+    };
+    res.send(sumRes);
 });
 
 
